d4d28f1109f8125e28926c499763e203b5c79bc4
Loh Chun Mun <chunmun_loh@live.com>
2022-07-05 16:39:44 +0800

    Section 8; Part 47

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
 
+    private CharStats[] playerStats;
+
+    public Text[] nameText, hpText, mpText, lvlText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +33,25 @@ public class GameMenu : MonoBehaviour
             else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+            }
+            else
+            {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
