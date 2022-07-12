e7b5405cead18c826629c0d018b78e2360972ab8
Loh Chun Mun <chunmun_loh@live.com>
2022-07-12 20:45:49 +0800

    Section 9; Part 54

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Type")]
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmor;
+
+    [Header("Item Details")]
+    public string itemName;
+    public string description;
+    public int value;
+    public Sprite itemSprite;
+
+    [Header("Item Details")]
+    public int amountToChange;
+    public bool affectHP, affectMP, affectStr;
+
+    [Header("Weapon/Armor Details")]
+    public int weaponStrength;
+    public int armorStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
