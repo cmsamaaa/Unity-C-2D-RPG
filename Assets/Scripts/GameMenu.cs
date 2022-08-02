db1756d02a5352f2a47d795daebb93b7e686dce6
Loh Chun Mun <chunmun_loh@live.com>
2022-08-02 17:43:56 +0800

    Section 9; Part 62

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -215,4 +215,10 @@ public class GameMenu : MonoBehaviour
     {
         itemCharChoiceMenu.SetActive(false);
     }
+
+    public void UseItem(int selectChar)
+    {
+        activeItem.Use(selectChar);
+        CloseItemCharChoice();
+    }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -34,4 +34,58 @@ public class Item : MonoBehaviour
     {
         
     }
+
+    public void Use(int charToUseOn)
+    {
+        CharStats selectedChar = GameManager.instance.playerStats[charToUseOn];
+
+        if (isItem)
+        {
+            if (affectHP)
+            {
+                selectedChar.currentHP += amountToChange;
+                selectedChar.currentHP = selectedChar.currentHP > selectedChar.maxHP ? selectedChar.maxHP : selectedChar.currentHP;
+
+                //if (selectedChar.currentHP > selectedChar.maxHP)
+                //{
+                //    selectedChar.currentHP = selectedChar.maxHP;
+                //}
+            }
+
+            if (affectMP)
+            {
+                selectedChar.currentMP += amountToChange;
+                selectedChar.currentMP = selectedChar.currentMP > selectedChar.maxMP ? selectedChar.maxMP : selectedChar.currentMP;
+            }
+
+            if (affectStr)
+            {
+                selectedChar.strength += amountToChange;
+            }
+        }
+
+        if (isWeapon)
+        {
+            if (selectedChar.equippedWeapon != "")
+            {
+                GameManager.instance.AddItem(selectedChar.equippedWeapon);
+            }
+
+            selectedChar.equippedWeapon = itemName;
+            selectedChar.weaponPower = weaponStrength;
+        }
+
+        if(isArmor)
+        {
+            if (selectedChar.equippedArmor != "") 
+            { 
+                GameManager.instance.AddItem(selectedChar.equippedArmor);
+            }
+
+            selectedChar.equippedArmor = itemName;
+            selectedChar.armorPower = armorStrength;
+        }
+
+        GameManager.instance.RemoveItem(itemName);
+    }
 }
