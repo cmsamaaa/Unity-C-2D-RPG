c3f50a9e9c85a9158887968bc4c93b32c404fff3
Loh Chun Mun <chunmun_loh@live.com>
2022-08-24 00:02:47 +0800

    Section 10; Part 67

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -13,6 +13,8 @@ public class Shop : MonoBehaviour
 
     public Text goldText;
 
+    public string[] itemsForSale;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/ShopKeeper.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+    private bool canOpen;
+
+    public string[] ItemsForSale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canOpen && Input.GetButtonDown("Fire1") && PlayerController.instance.canMove && !Shop.instance.shopMenu.activeInHierarchy)
+        {
+            Shop.instance.itemsForSale = ItemsForSale;
+
+            Shop.instance.OpenShop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpen = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpen = false;
+        }
+    }
+}
