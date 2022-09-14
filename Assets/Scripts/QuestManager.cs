4a197d80ea4aa9a9827cf900d381a6339d1827a7
Loh Chun Mun <chunmun_loh@live.com>
2022-09-14 15:43:17 +0800

    Section 11; Part 72

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -20,6 +20,45 @@ public class QuestManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(CheckIfComplete("quest test"));
+            MarkQuestComplete("quest test");
+            MarkQuestIncomplete("fight the demon");
+        }
+    }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for (int i = 0; i < questMarkerNames.Length; i++)
+        {
+            if (questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Quest " + questToFind + " does not exist");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {
+        if (GetQuestNumber(questToCheck) != 0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false;
     }
 }
