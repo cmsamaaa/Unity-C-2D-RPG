ea1e6fcf7fd0d556e069f857ae2f3a10807bab8d
Loh Chun Mun <chunmun_loh@live.com>
2022-09-14 16:10:44 +0800

    Section 11; Part 73

---
+++ Assets/Scripts/QuestObjectActivator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObjectActivator : MonoBehaviour
+{
+    public GameObject objectToActivate;
+
+    public string questToCheck;
+
+    public bool activateIfComplete;
+
+    private bool initialCheckDone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initialCheckDone)
+        {
+            initialCheckDone = true;
+
+            CheckCompletion();
+        }
+    }
+
+    public void CheckCompletion()
+    {
+        if (QuestManager.instance.CheckIfComplete(questToCheck))
+        {
+            objectToActivate.SetActive(activateIfComplete);
+        }
+    }
+}
