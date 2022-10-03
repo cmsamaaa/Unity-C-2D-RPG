9c4b08bab84b027af97ea7b61a7d310b2dabf73c
Loh Chun Mun <chunmun_loh@live.com>
2022-10-04 02:27:00 +0800

    Section 14; Part 84

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Current_Scene"))
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
