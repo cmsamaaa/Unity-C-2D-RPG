aa5d99b558594851ae731dbb6534526b45223127
Loh Chun Mun <chunmun_loh@live.com>
2022-10-25 02:58:47 +0800

    Section 15; Part 88

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+    public bool isPlayer;
+    public string[] movesAvailable;
+
+    public string charName;
+    public int currentHP, maxHP, currentMP, maxMP, strength, defence, wpnPower, armrPower;
+    public bool hasdDied;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
