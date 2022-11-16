c6aff68c391cf38b935a1af67646989a4d70a283
Loh Chun Mun <chunmun_loh@live.com>
2022-11-17 03:12:45 +0800

    Section 15; Part 99

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -191,6 +191,17 @@ public class BattleManager : MonoBehaviour
             GameManager.instance.battleActive = false;
             battleActive = false;
         }
+        else
+        {
+            while(activeBattlers[currentTurn].currentHP == 0)
+            {
+                currentTurn++;
+                if (currentTurn >= activeBattlers.Count)
+                {
+                    currentTurn = 0;
+                }
+            }
+        }
     }
 
     public IEnumerator EnemyMoveCo()
