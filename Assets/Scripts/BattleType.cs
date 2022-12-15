a1a2b7f578070d248108dd1e2563629083fe9fbf
Loh Chun Mun <chunmun_loh@live.com>
2022-12-16 04:51:49 +0800

    Section 15; Part 112

---
+++ Assets/Scripts/BattleType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class BattleType
+{
+    public string[] enemies;
+    public int rewardXP;
+    public string[] rewardItems;
+}
